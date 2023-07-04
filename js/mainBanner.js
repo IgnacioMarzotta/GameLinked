50c1f9cbd364b208413b323ef27c6165c49e8e86
Ignacio Marzotta <53620877+IgnacioMarzotta@users.noreply.github.com>
2023-07-03 23:03:03 -0400

    Add files via upload

---
+++ js/mainBanner.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.rev-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
